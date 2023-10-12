110cf1adfafcb108dbeec16e106e50e8112e2d39
kaikato <kaiykato@gmail.com>
2023-10-12 22:35:02 +0000

    Update changelog and add sdk tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+# 0.2.1
+
+Oct 12, 2023
+- Removes the default seed of synthesize (previously 0). When seed is unspecified, synthesize will now use a random seed.
+- The durations dictionary has been updated. Now includes each word itself and changes durations from # of samples to seconds.
+
 # 0.2.0
 
 Sep 7, 2023
---
+++ src/lmnt/test_api.py
@@ -0,0 +1,38 @@
+import pytest
+from api import Speech
+
+X_API_KEY = ""
+STAGING_URL = 'https://api.staging.lmnt.com'
+PROD_URL = 'https://api.lmnt.com'
+
+@pytest.fixture
+async def api():
+    async with Speech(X_API_KEY, _BASE_URL=STAGING_URL) as speech:
+        yield speech
+
+@pytest.mark.asyncio
+async def test_init(api):
+    assert api is not None
+
+@pytest.mark.asyncio
+async def test_list_voices(api):
+    voices = await api.list_voices()
+    assert isinstance(voices, dict)
+    assert len(voices) > 0
+
+@pytest.mark.asyncio
+async def test_synthesize_streaming(api):
+    voice = 'shanti'
+    result = await api.synthesize_streaming(voice)
+    assert result is not None
+
+@pytest.mark.asyncio
+async def test_durations(api):
+    voice = 'shanti'
+    text = 'Example Text'
+    result = await api.synthesize(text=text, voice=voice, durations=True)
+    assert result is not None
+    assert 'durations' in result
+    assert 'audio' in result
+    assert len(result['durations']) > 0
+    assert len(result['audio']) > 0
