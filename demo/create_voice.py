3498de167d4e0d3e2e9e5a3a1874708c1230b43e
kaikato <kaiykato@gmail.com>
2023-11-14 02:18:53 +0000

    Fix voice creation nits and default

--- demo/create_voice.py
+++ demo/create_voice.py
@@ -7,17 +7,17 @@ from lmnt.api import Speech
 load_dotenv() # Don't forget to add your LMNT API key to .env.
 
 async def main(args):
-    async with Speech(os.getenv('LMNT_API_KEY')) as s:
+    async with Speech(os.getenv('LMNT_API_KEY')) as speech:
         # Get the list of available voices.
-        voices = await s.list_voices()
+        voices = await speech.list_voices()
         print(voices)
 
         # Create a voice. Once completed, it will be available in your list of voices.
-        create_voice = await s.create_voice('test voice 3', True, args.inputFiles)
+        create_voice = await speech.create_voice('Demo voice', True, args.input_files)
         print(create_voice)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Create a voice using LMNT API')
-    parser.add_argument('-i', '--inputFiles', nargs='+', required=False, default='sample-audio-input.mp3', help='Input file path')
+    parser.add_argument('-i', '--input_files', nargs='+', required=False, default=['sample-audio-input.mp3'], help='Input file path')
     args = parser.parse_args()
     asyncio.run(main(args))
