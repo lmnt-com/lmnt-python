85f0b63ccdffb0518f8d189366f3da9bb142b805
Walter Korman <shaper@lmnt.com>
2023-11-21 17:55:24 -0800

    Apply ruff and autopep8 auto-formatting. (#14)

--- demo/synthesize.py
+++ demo/synthesize.py
@@ -4,27 +4,28 @@ import os
 from dotenv import load_dotenv
 from lmnt.api import Speech
 
-load_dotenv() # Don't forget to add your LMNT API key to .env.
+load_dotenv()  # Don't forget to add your LMNT API key to .env.
+
 
 async def main(args):
-    async with Speech(os.getenv('LMNT_API_KEY')) as s:
-        #  Get the list of available voices.
-        voices = await s.list_voices()
-        print(voices)
+  async with Speech(os.getenv('LMNT_API_KEY')) as s:
+    #  Get the list of available voices.
+    voices = await s.list_voices()
+    print(voices)
 
-        # Get your account information.
-        account = await s.account_info()
-        print(account)
+    # Get your account information.
+    account = await s.account_info()
+    print(account)
 
-        # Synthesize text to speech.
-        synthesize = await s.synthesize(text=args.text, voice=args.voice)
-        with open('output.mp3', 'wb') as f:
-            f.write(synthesize['audio'])
-        print('Done.')
+    # Synthesize text to speech.
+    synthesize = await s.synthesize(text=args.text, voice=args.voice)
+    with open('output.mp3', 'wb') as f:
+      f.write(synthesize['audio'])
+    print('Done.')
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Synthesize text to speech using LMNT API')
-    parser.add_argument('-t', '--text', required=False, default='This is a test of the LMNT API.', help='Text to synthesize')
-    parser.add_argument('-v', '--voice', required=False, default='curtis', help='Voice to use')
-    args = parser.parse_args()
-    asyncio.run(main(args))
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Synthesize text to speech using LMNT API')
+  parser.add_argument('-t', '--text', required=False, default='This is a test of the LMNT API.', help='Text to synthesize')
+  parser.add_argument('-v', '--voice', required=False, default='curtis', help='Voice to use')
+  args = parser.parse_args()
+  asyncio.run(main(args))
