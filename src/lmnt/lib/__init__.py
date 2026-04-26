f848612d1c0e9747c053b655fc51eac38635626e
Zach Johnson <zach@lmnt.com>
2026-04-26 23:32:25 +0000

    fix: extend lmnt.lib path for sibling distributions

--- src/lmnt/lib/__init__.py
+++ src/lmnt/lib/__init__.py
@@ -1,3 +1,7 @@
+from pkgutil import extend_path
+
+__path__ = extend_path(__path__, __name__)
+
 from .websocket_streaming import Duration, SpeechSession, SpeechSessionResponse
 
 __all__ = ["SpeechSession", "SpeechSessionResponse", "Duration"]
